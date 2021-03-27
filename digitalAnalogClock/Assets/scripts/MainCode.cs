fae1f9573a6c896a665c4e77d9ab11f22eaed5ee
Mohammed Abudaf <58439755+Mohammad-Abudaf@users.noreply.github.com>
2021-03-27 14:28:43 +0300

    Update MainCode.cs

--- digitalAnalogClock/Assets/scripts/MainCode.cs
+++ digitalAnalogClock/Assets/scripts/MainCode.cs
@@ -1,35 +1,36 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class MainCode : MonoBehaviour{
-    public Image secondsHand;
-    public Image minHand;
-    public Image hoursHand;
+    public Image SecondsHand;
+    public Image MinHand;
+    public Image HoursHand;
 
     public Text outputTimeAsDigital;
 
-    private float _currentSec;
-    private float _currentMin;
-    private float _currentHour;
-    private float _totalSecond;
+    private int CurrentSeconds;
+    private int CurrentMin;
+    private int CurrentHour;
+
     private void Update(){
-        _totalSecond = Time.time;
-        _currentSec = _totalSecond % 60;
-        _currentMin = (_totalSecond / 60) % 60;
-        _currentHour = (_totalSecond / 3600) % 24;
+        CurrentSeconds = DateTime.Now.Second;
+        CurrentMin = DateTime.Now.Minute;
+        CurrentHour = DateTime.Now.Hour;
 
-        secondsHand.gameObject.transform.rotation =
-            Quaternion.Euler(new Vector3(0, 0, -6 * (float)Math.Truncate(_currentSec)));
-        minHand.gameObject.transform.rotation =
-            Quaternion.Euler(new Vector3(0, 0, -6 * _currentMin));
-        hoursHand.gameObject.transform.rotation =
-            Quaternion.Euler(new Vector3(0, 0, -6 * _currentHour));
+        SecondsHand.gameObject.transform.rotation =
+            Quaternion.Euler(new Vector3(0, 0, -6 * CurrentSeconds));
+        MinHand.gameObject.transform.rotation =
+            Quaternion.Euler(new Vector3(0, 0, -6 * CurrentMin));
+        HoursHand.gameObject.transform.rotation =
+            Quaternion.Euler(new Vector3(0, 0, -30 * CurrentHour));
 
-        outputTimeAsDigital.text = $"{(int)_currentHour:00}:{(int)_currentMin:00}:{(int)_currentSec:00}";
+        outputTimeAsDigital.text = $"{CurrentHour:00}:{CurrentMin:00}:{CurrentSeconds:00}";
     }
 
     public void ExitBtn(){
-        print("the application will quit now");
+        print("the application will Quit");
         Application.Quit();
     }
 }
+
